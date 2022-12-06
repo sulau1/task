cbb0d980186dbc1face76096288716a3a3fdce90
sulau1 <99355115+sulau1@users.noreply.github.com>
2022-12-06 20:37:42 +0300

    Add files via upload

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const chalk_1 = __importDefault(require("chalk"));
+let myDate = new Date();
+console.log(chalk_1.default.blue("My date and time is = " + myDate));
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import chalk from "chalk";
+let myDate: Date = new Date();
+console.log(chalk.blue("My date and time is = " + myDate));
